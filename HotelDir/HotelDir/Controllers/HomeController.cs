c39d8993e8e080218902827d64de23a9246d2386
Emily Maycock <emmaycock@gmail.com>
2012-06-14 13:32:30 +0100

    Adding HotelDir file
    First version of the project.

---
+++ HotelDir/HotelDir/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelDir.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult Hotel()
+
+        {
+            ViewData["Title"] = "Hotel";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ HotelDir/HotelDir/Views/Home/Hotel.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Home Page
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: ViewData["Message"] %></h2>
+    <p>testing class exercise ... this will be removed.  </p>
+</asp:Content>
