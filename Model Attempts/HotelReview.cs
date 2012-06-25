df41b674012c659e51aa6b1d84df1c04ee2dec21
Brian Raymond <brianraymond29@gmail.com>
2012-06-25 23:18:37 +0100

    Model Hotel Review

---
+++ Model Attempts/HotelReview.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Linq.Mapping;
+
+namespace ModelHotel
+{
+    public class HotelReview
+    {
+        [Column(IsPrimaryKey = true, Storage = "review_id")]
+        public string review_id;
+
+        public String HotelName;
+        [Column]
+        public String Hotelurl;
+        [Column]
+        public String Street;
+        [Column]
+        public String City;
+        [Column]
+        public String State;
+        [Column]
+        public int Value;
+        [Column]
+        public int Comfort;
+        [Column]
+        public int Overallrating;
+        [Column]
+    
+    }
+}
