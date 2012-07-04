8c028c5543a8202be2ac8000baa69521285b2b0f
Brian Raymond <brianraymond29@gmail.com>
2012-07-04 19:02:10 +0100

    Two extra datasets

---
+++ HotelDir/HotelDir/Models/HotelDetailsDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using HotelDir.Models;
+
+
+namespace HotelDir.Models
+{
+    public class HotelDetailsDB : DbContext
+    {
+   
+        public DbSet<HotelDetails> hoteldetails { get; set; }
+    }
+}
+
