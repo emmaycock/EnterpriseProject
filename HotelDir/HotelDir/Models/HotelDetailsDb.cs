eff1b98dd113f47cc01f511de96a6cfe3693cd04
Stephen O'Rourke <steveor83@hotmail.com>
2012-07-07 15:49:39 +0100

    New class for DB connectivity
    - Added HotelDetailsDb class for Database connectivity (not working yet)
    - Added ui folder containing HotelUI class

---
+++ HotelDir/HotelDir/Models/HotelDetailsDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using HotelDir.Models.ui;
+
+//Database connectivity class
+namespace HotelDir.Models
+{
+    public class HotelDetailsDb : DbContext
+    {
+        public DbSet<HotelDetails> hotelDetails { get; set; }
+
+        public DbSet<HotelUI> HotelUIs { get; set; }
+    }
+}
