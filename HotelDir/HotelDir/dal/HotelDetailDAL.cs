d226070b4560ff52553bf648624118fcf678f1f5
Stephen O'Rourke <steveor83@hotmail.com>
2012-07-10 19:14:06 +0100

    Added DAL and new class
    10/07/12
    
    - Installed EntityFramework package via Package Manager Console.
    'HotelDetailsDb.cs' errors now resolved.
    - Added 'dal' folder with 'HotelDetailsDAL' class for handling Data
    Access Layer, including reference dependencies

---
+++ HotelDir/HotelDir/dal/HotelDetailDAL.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HotelDir.Models;
+using System.Configuration;
+
+//Class for handling Data Access Layer
+namespace HotelDir.dal
+{
+    public class HotelDetailsDAL
+    {
+        //Access the DB
+        protected HotelDetailsDb db = new HotelDetailsDb();
+
+        public HotelDetailsDAL()
+        {
+            ConnectionStringSettingsCollection connections = ConfigurationManager.ConnectionStrings;
+            if (connections.Count != 0)
+            {
+                Console.WriteLine();
+                System.Diagnostics.Debug.WriteLine("Using ConnectionStrings property.");
+
+
+                //get the collection elements.
+                foreach (ConnectionStringSettings connection in
+                    connections)
+                {
+                    string name = connection.Name;
+                    string provider = connection.ProviderName;
+                    string connectionString = connection.ConnectionString;
+
+                    System.Diagnostics.Debug.WriteLine("Name:                {0}",
+                        name);
+                    System.Diagnostics.Debug.WriteLine(" Connection string:  {0}",
+                        connectionString);
+                    System.Diagnostics.Debug.WriteLine("Provider:           {0}",
+                       provider);
+
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("No connection string is defined.");
+            }
+        }
+
+        //Method defined in logic layer is called here in the DAL
+        /*
+        public HotelDetails <Method>(param1, param2)
+        {
+           return null;
+
+        }
+         */
+
+        public HotelDetails findHotelDetailsById(int id)
+        {
+            return null;
+        }
+
+        public HotelDetails addHotelDetails(HotelDetails hotel)
+        {
+            return null;
+        }
+
+    }
+}
