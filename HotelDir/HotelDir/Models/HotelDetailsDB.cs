3a287702243629e9285b224f413a5037fb3f9d66
Brian Raymond <brianraymond29@gmail.com>
2012-07-11 21:43:45 +0100

    Ignore

---
+++ HotelDir/HotelDir/Models/HotelDetailsDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using HotelDir.Models.ui;
+
+namespace HotelDir.Models
+{
+    namespace HotelDir.Models
+    {
+        public class HotelDetailsDb : DbContext
+        {
+            public DbSet<HotelDetail> hotelDetails { get; set; }
+
+            public DbSet<HotelUI> HotelUIs { get; set; }
+        }
+    }
+}
