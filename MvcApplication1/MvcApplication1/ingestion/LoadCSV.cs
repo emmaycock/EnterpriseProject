997c5203038cd182050b773d936a5c3c29651137
James Peyton <jamesgpeyton@gmail.com>
2012-07-21 13:29:30 +0100

    CSV parser

---
+++ MvcApplication1/MvcApplication1/ingestion/LoadCSV.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using MvcApplication1.Models;
+using System.Reflection;
+using MvcApplication1.ingestion;
+
+
+namespace MvcApplication1
+{
+   public class InitialiseCSV
+   {
+       public IDataParser csvparse = new londoncsvparser();
+       string csvpath =("C:\\Users\\-\\Desktop\\MvcApplication1\\Data\\HotelCSVData.csv");
+
+       public List<HotelDetail> importHotelCSVData()
+       {
+           List<HotelDetail> hoteldetails = new List<HotelDetail>();
+           StreamReader reader = new StreamReader(csvpath, true);
+           csvparse.setStreamSource(reader);
+           hoteldetails = csvparse.parseHotelDetails();
+           return hoteldetails;
+       }
+
+     
+
+   }
+}
+ 
